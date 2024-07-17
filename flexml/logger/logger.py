4e89915387839d91c0f07b2945733ef7108288d6
ozguraslank <ozguraslank@gmail.com>
2024-07-17 11:17:35 +0300

    Refactored logger code

--- flexml/logger/logger.py
+++ flexml/logger/logger.py
@@ -6,36 +6,24 @@ __LOG_FILE_PATH = os.path.join(__LOG_DIR_PATH, "flexml_logs.log")
 
 def _logger_configuration(logging_to_file: bool = True):
     """
-    Configures the logger to save logs to a file or not
-
+    Configures the logger to save logs to a file or not.
+    
     Parameters
     ----------
     logging_to_file : bool, (default=True)
-        If True, the logs will be saved to a file in the current path, located in /logs/flexml_logs.log, Otherwise, it will not be saved.
+        If True, logs are saved to /logs/flexml_logs.log. Otherwise, logs are not saved to a file.
     """
+    handlers = [logging.StreamHandler()]
     if logging_to_file:
-        # Create the log directory if it doesn't exist
-        try:
-            if not os.path.exists(__LOG_DIR_PATH):
-                os.makedirs(__LOG_DIR_PATH)
-        except Exception as e:
-            print(f"Error creating log directory, no logs will be saved into a .log file, Error: {e}")
+        os.makedirs(__LOG_DIR_PATH, exist_ok=True)
+        handlers.append(logging.FileHandler(__LOG_FILE_PATH))
 
-        logging.basicConfig(level="INFO",
-                            format='%(levelname)s | %(asctime)-3s | %(name)s.%(funcName)s | %(message)-3s',
-                            datefmt="%Y-%m-%d %H:%M:%S",
-                            handlers=[
-                                logging.FileHandler(__LOG_FILE_PATH),
-                                logging.StreamHandler()
-                            ])
-        
-    else:
-        logging.basicConfig(level="INFO",
-                            format='%(levelname)s | %(asctime)-3s | %(name)s.%(funcName)s | %(message)-3s',
-                            datefmt="%Y-%m-%d %H:%M:%S",
-                            handlers=[
-                                logging.StreamHandler()
-                            ])
+    logging.basicConfig(
+        level="INFO",
+        format='%(levelname)s | %(asctime)-3s | %(name)s.%(funcName)s | %(message)-3s',
+        datefmt="%Y-%m-%d %H:%M:%S",
+        handlers=handlers
+    )
 
 def get_logger(name: str, logging_to_file: bool = True) -> logging.Logger:
     """
