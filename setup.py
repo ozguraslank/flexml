f43afd4af4564ef57c6ff1ab168ef9fee525456f
Ozgur Aslan <ozguraslank@gmail.com>
2024-08-27 23:22:02 +0300

    Revised publish name in setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="FlexML",
+    name="flexml",
     version="0.1.0",
     author="Ozgur Aslan",
     author_email="ozguraslank@gmail.com",
