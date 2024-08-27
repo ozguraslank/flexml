5a2a73af0e0934a167abd5821f79a7f8de45a25a
Ozgur Aslan <ozguraslank@gmail.com>
2024-08-28 00:33:12 +0300

    Released 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="flexml",
-    version="0.1.0",
+    version="1.0.1",
     author="Ozgur Aslan",
     author_email="ozguraslank@gmail.com",
     description="Easy-to-use and flexible AutoML library for Python",
