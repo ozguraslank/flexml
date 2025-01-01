efc9f8f5c6e2eefdc47f8b24d876e5c88db7e84f
Ozgur Aslan <ozguraslank@gmail.com>
2025-01-01 18:52:01 +0300

    Improved supervise cross-validation experiments' unit tests * n_folds and group amount are decrased from 5 to 3 * test_size increased from 0.20 to 0.25 * Removed unnecessary param definitions in certain cv methods * Speed up to 20-50%

--- tests/test_cross_validation.py
+++ tests/test_cross_validation.py
@@ -15,22 +15,22 @@ class TestCrossValidation(unittest.TestCase):
     breast_data = load_breast_cancer(as_frame=True)
     classification_data = breast_data['frame']
     classification_data["target"] = breast_data['target']
-    classification_data["group"] = classification_data.index % 5  # Add synthetic group column
-    regression_data["group"] = regression_data.index % 5  # Add synthetic group column
+    classification_data["group"] = classification_data.index % 3  # Add synthetic group column
+    regression_data["group"] = regression_data.index % 3  # Add synthetic group column
 
     @parameterized.expand([
-        ("Classification", "kfold", {"n_splits": 5, "test_size": None, "groups_col": None}),
-        ("Classification", "stratified_kfold", {"n_splits": 5, "test_size": None, "groups_col": None}),
-        ("Classification", "shuffle_split", {"n_splits": 5, "test_size": 0.2, "groups_col": None}),
-        ("Classification", "stratified_shuffle_split", {"n_splits": 5, "test_size": 0.2, "groups_col": None}),
-        ("Classification", "group_kfold", {"n_splits": 5, "test_size": None, "groups_col": "group"}),
-        ("Classification", "group_shuffle_split", {"n_splits": 5, "test_size": 0.2, "groups_col": "group"}),
-        ("Classification", "holdout", {"n_splits": None, "test_size": 0.2, "groups_col": None}),
-        ("Regression", "kfold", {"n_splits": 5, "test_size": None, "groups_col": None}),
-        ("Regression", "shuffle_split", {"n_splits": 5, "test_size": 0.2, "groups_col": None}),
-        ("Regression", "group_kfold", {"n_splits": 5, "test_size": None, "groups_col": "group"}),
-        ("Regression", "group_shuffle_split", {"n_splits": 5, "test_size": 0.2, "groups_col": "group"}),
-        ("Regression", "holdout", {"n_splits": None, "test_size": 0.2, "groups_col": None}),
+        ("Classification", "kfold", {"n_splits": 3}),
+        ("Classification", "stratified_kfold", {"n_splits": 3}),
+        ("Classification", "shuffle_split", {"n_splits": 3, "test_size": 0.25}),
+        ("Classification", "stratified_shuffle_split", {"n_splits": 3, "test_size": 0.25}),
+        ("Classification", "group_kfold", {"n_splits": 3, "groups_col": "group"}),
+        ("Classification", "group_shuffle_split", {"n_splits": 3, "test_size": 0.25, "groups_col": "group"}),
+        ("Classification", "holdout", {"test_size": 0.25}),
+        ("Regression", "kfold", {"n_splits": 3}),
+        ("Regression", "shuffle_split", {"n_splits": 3, "test_size": 0.25}),
+        ("Regression", "group_kfold", {"n_splits": 3, "groups_col": "group"}),
+        ("Regression", "group_shuffle_split", {"n_splits": 3, "test_size": 0.2, "groups_col": "group"}),
+        ("Regression", "holdout", {"test_size": 0.25}),
     ])
     def test_cross_validation(
         self,
@@ -66,4 +66,4 @@ class TestCrossValidation(unittest.TestCase):
             n_folds=params.get("n_splits"),
             test_size=params.get("test_size"),
             groups_col=params.get("groups_col")
-            )
+        )
